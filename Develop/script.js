98fb9afa26b5388b34757cbc95bd73278b4f6bb0
Dooski <dmckeon495@gmail.com>
2020-03-09 12:44:12 -0400

    set up button functionality; began if loops with length

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +14,19 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Generate password for writePassword function
+
+function generatePassword(password) {
+  if (password !== null) {
+    length = prompt("How long would you like your fantastic password to be? (Minimum of 8, maximum of 128")
+  }
+
+  password = length;
+  return password;
+}
+
+
+
+
+
