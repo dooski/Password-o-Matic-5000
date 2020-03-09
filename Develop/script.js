c77d92cdc555195e4fa6d79dd8002d9561113654
Dooski <dmckeon495@gmail.com>
2020-03-09 14:03:07 -0400

    if loops for length now requires var length to be between 8 and 128

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
+// Variables for letters, symbols, uppercase and lowercase
+var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var symbols = "?"
 
 // Write password to the #password input
 function writePassword() {
@@ -19,7 +22,10 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(password) {
   if (password !== null) {
-    length = prompt("How long would you like your fantastic password to be? (Minimum of 8, maximum of 128")
+    for (var length = null; length === null || (length < 8 || length > 128) || uppercaseLetters.includes(length) || lowercaseLetters.includes(length); length = length) {
+      length = prompt("How long would you like your fantastic password to be? (Minimum of 8, maximum of 128)");
+    }
+    for (var )
   }
 
   password = length;
